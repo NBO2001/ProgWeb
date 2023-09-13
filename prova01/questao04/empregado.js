dd9c6898d4a77ed1c5bc47b93c7acff860c2c7bb
Natanael Bezerra de Oliveira <54229396+NBO2001@users.noreply.github.com>
2023-09-13 08:58:59 -0400

    Questao 4, feita

---
+++ prova01/questao04/empregado.js
@@ -0,0 +1,66 @@
+
+class Empregado{
+
+    #nome;
+    #salario;
+
+    constructor(nome, salario){
+        this.#nome = nome;
+        this.#salario = salario;
+    }
+
+    get Salario(){
+        return this.#salario;
+    }
+
+    set Salario(salarioNovo){
+
+        if(salarioNovo < 0){
+            console.log("Erro, valor negativo");
+        }else{
+            this.#salario = salarioNovo;
+        }
+
+    }
+
+    toString(){
+        let string = `Empregado(nome: ${this.#nome}, salario: ${this.#salario})`;
+        return string;
+    }
+
+    static somaSalarios(empregados){
+
+        let sum = 0;
+        
+        empregados.map( e => {
+            sum = sum + e.Salario;
+        });
+        
+        return sum;
+    }
+
+}
+
+let empregado01 = new Empregado("Natanael B.", 10500);
+let empregado02 = new Empregado("Beatriz E.", 10000);
+
+let empregados = [empregado01,empregado02];
+
+// Valores iniciais
+empregados.map( e => console.log(e.toString()));
+
+// Aumentando 10%
+empregados.map( e => {
+
+    let aumento = e.Salario * 0.10;
+    let newSalario = e.Salario + aumento;
+    e.Salario = newSalario;
+})
+
+// Valores apos o aumento
+empregados.map( e => console.log(e.toString()));
+
+
+// Soma dos salarios
+let somaSalarioTol = Empregado.somaSalarios(empregados);
+console.log(`Total dos salarios: ${somaSalarioTol}`)
